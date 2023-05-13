54179de130e3f485200b972ede288830b9a2695b
Rukmini Jadhav <rukminibiradar7@gmail.com>
2023-05-13 12:42:51 +0530

    added

---
+++ MoodAnalysisException.java
@@ -0,0 +1,19 @@
+package day21Programming;
+
+public class MoodAnalysisException extends Exception {
+
+	public static final String TypeOfException = null;
+	String type;
+
+    
+    enum TypeOfException {
+      ENTERED_EMPTY,  ENTERED_NULL;
+    	
+    }
+
+    public MoodAnalysisException(String message,String typeofexception2) {
+        super(message);
+        this.type = typeofexception2;
+    }
+
+}
